Added explanations and corrected some units

star_structure.py
@@ -5,22 +5,26 @@ from scipy.interpolate import CubicSpline
 
 class Star:
 
+    # Defining some constants
+    SOLAR_MASS = 1.48e3             # Solar mass [m]
+    SOLAR_RADIUS = 6.957e8          # Solar radius [m]
+
     def __init__(self, rho_eos, p_center, p_surface):
 
         # Set the density function as the EOS given (rho(p))
         self.rho = rho_eos
 
-        # Set the integration constants: pressure, mass, and density at r=0
-        self.p_0 = p_center
-        self.m_0 = 0.0
-        self.rho_0 = self.rho(self.p_0)
+        # Set the integration constants: pressure, mass, and density at r=0, at the center
+        self.p_0 = p_center                     # Center pressure [m^-2]
+        self.m_0 = 0.0                          # Center mass [m]
+        self.rho_0 = self.rho(self.p_0)         # Center energy density [m^-2]
 
         # Set the boundary value for the termination of the ODE integration: pressure at r=R, on the surface
-        self.p_surface = p_surface
+        self.p_surface = p_surface              # Surface pressure [m^-2]
 
         # Initialize star properties: radius and total mass
-        self.star_radius = 0.0
-        self.star_mass = 0.0
+        self.star_radius = 0.0                  # Star radius [m]
+        self.star_mass = 0.0                    # Star mass [m]
 
     def _ode_system(self, r, y):
 
@@ -54,8 +58,6 @@ class Star:
             # Get the star radius and mass from the ODE termination event
             self.star_radius = ode_solution.t_events[0][0]
             self.star_mass = ode_solution.y_events[0][0][1]
-            print(f"Star radius = {self.star_radius}")
-            print(f"Star mass = {self.star_mass}")
 
         # Create interpolated functions for the solution using CubicSpline
         self.p_spline_function = CubicSpline(r_ode_solution, p_ode_solution)
@@ -68,15 +70,19 @@ class Star:
         self.m_num_solution = self.m_spline_function(self.r_space)
         self.rho_num_solution = self.rho_spline_function(self.r_space)
 
-    def plot_result(self):
+    def show_result(self):
+
+        # Print the star radius and mass
+        print(f"Star radius = {self.star_radius/self.SOLAR_RADIUS} [solar radius]")
+        print(f"Star mass = {self.star_mass/self.SOLAR_MASS} [solar mass]")
 
         # Show simple plot of the solution
         plt.figure()
-        plt.plot(self.r_space, self.p_num_solution, linewidth=1, label='pressure')
-        plt.plot(self.r_space, self.m_num_solution, linewidth=1, label='mass function')
-        plt.plot(self.r_space, self.rho_num_solution, linewidth=1, label='density')
-        plt.title('TOV solution for the star')
-        plt.xlabel('r')
+        plt.plot(self.r_space, self.p_num_solution*10**8, linewidth=1, label="pressure [10^8 m^-2]")
+        plt.plot(self.r_space, self.m_num_solution/self.SOLAR_MASS, linewidth=1, label="mass function [solar mass]")
+        plt.plot(self.r_space, self.rho_num_solution*10**8, linewidth=1, label="density [10^8 m^-2]")
+        plt.title("TOV solution for the star")
+        plt.xlabel("r [m]")
         plt.legend()
         plt.show()
 
@@ -85,15 +91,15 @@ if __name__ == "__main__":
 
     # Set the EOS and pressure at the center and surface of the star
     def rho(p):
-        c = 1.0e12          # [cm^2]
+        c = 1.0e8           # [m^2]
         return (np.abs(p/c))**(1/2)
 
     def p(rho):
-        c = 1.0e12          # [cm^2]
+        c = 1.0e8           # [m^2]
         return c*rho**2
 
-    rho_center = 2.376364e-13           # Center density in GU [cm^-2]
-    p_center = rho(rho_center)          # Center pressure in GU [cm^-2]
+    rho_center = 2.376364e-9            # Center density in GU [m^-2]
+    p_center = rho(rho_center)          # Center pressure in GU [m^-2]
     p_surface = 0.0
 
     # Define the object
@@ -102,5 +108,5 @@ if __name__ == "__main__":
     # Solve the TOV equation
     star_object.solve_tov()
 
-    # Plot the result
-    star_object.plot_result()
+    # Show the result
+    star_object.show_result()
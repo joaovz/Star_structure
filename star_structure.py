c49e693b65f6d5238f33ac7a346b16f869e00e9e
joaovz <joaovzastrow@gmail.com>
2023-03-16 16:10:11 -0300

    Small changes to improve readability

--- star_structure.py
+++ star_structure.py
@@ -9,8 +9,8 @@ class Star:
     """
 
     # Defining some constants
-    SOLAR_MASS = 1.48e3             # Solar mass [m]
-    SOLAR_RADIUS = 6.957e8          # Solar radius [m]
+    SOLAR_MASS = 1.48e3         # Solar mass [m]
+    SOLAR_RADIUS = 6.957e8      # Solar radius [m]
 
     def __init__(self, rho_eos, p_center, p_surface):
         """Initialization method
@@ -25,16 +25,16 @@ class Star:
         self.rho = rho_eos
 
         # Set the integration constants: pressure, mass, and density at r=0, at the center
-        self.p_0 = p_center                     # Center pressure [m^-2]
-        self.m_0 = 0.0                          # Center mass [m]
-        self.rho_0 = self.rho(self.p_0)         # Center energy density [m^-2]
+        self.p_0 = p_center                 # Center pressure [m^-2]
+        self.m_0 = 0.0                      # Center mass [m]
+        self.rho_0 = self.rho(self.p_0)     # Center energy density [m^-2]
 
         # Set the boundary value for the termination of the ODE integration: pressure at r=R, on the surface
-        self.p_surface = p_surface              # Surface pressure [m^-2]
+        self.p_surface = p_surface          # Surface pressure [m^-2]
 
         # Initialize star properties: radius and total mass
-        self.star_radius = 0.0                  # Star radius [m]
-        self.star_mass = 0.0                    # Star mass [m]
+        self.star_radius = 0.0              # Star radius [m]
+        self.star_mass = 0.0                # Star mass [m]
 
     def _ode_system(self, r, y):
         """Method that implements the TOV ODE system in the form ``dy/dr = f(r, y)``, used by the IVP solver
@@ -51,8 +51,8 @@ class Star:
         p = y[0]
         m = y[1]
         rho = self.rho(p)
-        dp_dr = -((rho + p) * (m + 4 * np.pi * r**3 * p)) / (r * (r - 2 * m))           # TOV equation
-        dm_dr = 4 * np.pi * r**2 * rho                                                  # Rate of change of the mass function
+        dp_dr = -((rho + p) * (m + 4 * np.pi * r**3 * p)) / (r * (r - 2 * m))       # TOV equation
+        dm_dr = 4 * np.pi * r**2 * rho                                              # Rate of change of the mass
         return [dp_dr, dm_dr]
 
     def _ode_termination_event(self, r, y):
@@ -67,8 +67,8 @@ class Star:
             float: ``p - p_surface``
         """
 
-        return y[0] - self.p_surface                # Condition of the event (event happens when condition == 0 ==> when p == p_surface)
-    _ode_termination_event.terminal = True          # Set the event as a terminal event, terminating the integration of the ODE
+        return y[0] - self.p_surface            # Condition of the event: trigger when condition == 0 (p == p_surface)
+    _ode_termination_event.terminal = True      # Set the event as a terminal event, terminating the integration of the ODE
 
     def solve_tov(self, r_begin=np.finfo(float).eps, r_end=np.inf, r_nsamples=10**6, method='RK45'):
         """Method that solves the TOV system for the star, finding the functions p(r), m(r), and rho(r)
@@ -85,7 +85,7 @@ class Star:
         """
 
         # Solve the ODE system
-        ode_solution = solve_ivp(self._ode_system, [r_begin, r_end], [self.p_0, self.m_0], method=method, events=[self._ode_termination_event])
+        ode_solution = solve_ivp(self._ode_system, [r_begin, r_end], [self.p_0, self.m_0], method, events=[self._ode_termination_event])
         r_ode_solution = ode_solution.t
         p_ode_solution = ode_solution.y[0]
         m_ode_solution = ode_solution.y[1]
@@ -94,12 +94,11 @@ class Star:
         # Check the ODE solution status, and treat each case
         if ode_solution.status == -1:
             raise Exception(ode_solution.message)
-        elif ode_solution.status == 0:
+        elif ode_solution.status != 1:
             raise Exception("The solver did not find the ODE termination event")
-        elif ode_solution.status == 1:
-            # Get the star radius and mass from the ODE termination event
-            self.star_radius = ode_solution.t_events[0][0]
-            self.star_mass = ode_solution.y_events[0][0][1]
+        # Get the star radius and mass from the ODE termination event
+        self.star_radius = ode_solution.t_events[0][0]
+        self.star_mass = ode_solution.y_events[0][0][1]
 
         # Create interpolated functions for the solution using CubicSpline
         self.p_spline_function = CubicSpline(r_ode_solution, p_ode_solution)
@@ -136,15 +135,15 @@ if __name__ == "__main__":
 
     # Set the EOS and pressure at the center and surface of the star
     def rho(p):
-        c = 1.0e8           # [m^2]
+        c = 1.0e8       # [m^2]
         return (np.abs(p / c))**(1 / 2)
 
     def p(rho):
-        c = 1.0e8           # [m^2]
+        c = 1.0e8       # [m^2]
         return c * rho**2
 
-    rho_center = 2.376364e-9            # Center density in GU [m^-2]
-    p_center = rho(rho_center)          # Center pressure in GU [m^-2]
+    rho_center = 2.376364e-9        # Center density [m^-2]
+    p_center = rho(rho_center)      # Center pressure [m^-2]
     p_surface = 0.0
 
     # Define the object

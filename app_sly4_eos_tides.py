5a856063aad8a66a69a4d6d8328560ac2892dcbb
joaovz <joaovzastrow@gmail.com>
2023-08-25 11:45:07 -0300

    Added the app to test the SLy4 EOS
    * Added the app files for TOV and tidal tests;
    * Added the EOS table file;
    * Added the generated graphs.

---
+++ app_sly4_eos_tides.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from data_handling import *
+from perfect_fluid_star_family_tides import DeformedStarFamily
+from eos_library import TableEOS
+
+
+# Create the EOS object
+eos = TableEOS(fname='data/SLy4.dat')
+
+# Set the pressure at the center and surface of the star
+p_center = eos.p_center     # Center pressure [m^-2]
+p_surface = 1e-22           # Surface pressure [m^-2]
+
+# Print the values used for p_center and p_surface
+print(f"p_center = {p_center} [m^-2]")
+print(f"p_surface = {p_surface} [m^-2]")
+
+# Set the p_center space that characterizes the star family
+p_center_space = p_center * np.logspace(-3.5, 0.0, 50)
+
+# Create the star family object
+star_family_object = DeformedStarFamily(eos.rho, p_center_space, p_surface)
+
+# Solve the TOV equation, and the tidal equation
+star_family_object.solve_tidal(max_step=100.0)
+
+# Plot the calculated radius-mass curve
+star_family_object.plot_radius_mass_curve()
+
+# Plot the calculated k2 curve
+star_family_object.plot_k2_curve()
---
+++ app_sly4_eos_tov.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from data_handling import *
+from star_structure import Star
+from star_family import StarFamily
+from eos_library import TableEOS
+
+
+# Create the EOS object
+eos = TableEOS(fname='data/SLy4.dat')
+
+# Set the pressure at the center and surface of the star
+p_center = eos.p_center     # Center pressure [m^-2]
+p_surface = 1e-22           # Surface pressure [m^-2]
+
+# Print the values used for p_center and p_surface
+print(f"p_center = {p_center} [m^-2]")
+print(f"p_surface = {p_surface} [m^-2]")
+
+# Single star
+
+# Define the object
+star_object = Star(eos.rho, p_center, p_surface)
+
+# Solve the TOV equation
+star_object.solve_tov(max_step=100.0)
+
+# Plot the star structure curves
+star_object.plot_star_structure_curves()
+
+# Star Family
+
+# Set the p_center space that characterizes the star family
+p_center_space = p_center * np.logspace(-3.5, 0.0, 50)
+
+# Create the star family object
+star_family_object = StarFamily(eos.rho, p_center_space, p_surface)
+
+# Solve the TOV equation
+star_family_object.solve_tov(max_step=100.0)
+
+# Plot the calculated radius-mass curve
+star_family_object.plot_radius_mass_curve()
